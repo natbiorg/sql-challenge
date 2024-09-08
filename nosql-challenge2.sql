bc83722492cb9b748d91737763ded4effc659951
Natalie <natalierg1199@gmail.com>
2024-09-07 23:04:18 -0500

    finished as much possible

--- nosql-challenge2.sql
+++ nosql-challenge2.sql
@@ -119,15 +119,33 @@ JOIN
 
 /*5 List first name, last name, and sex of each employee 
 	whose first name is Hercules and whose last name begins with the letter B. */
+SELECT first_name, last_name, sex 
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 /*6 List each employee in the Sales department, including their employee number, 
 	last name, and first name.*/
+SELECT emp_no, last_name, first_name
+FROM employees
+WHERE emp_no IN
+(
+  SELECT emp_no
+  FROM dept_emp
+  WHERE dept_no = 'd007'
+);
 
 /*7 List each employee in the Sales and Development departments, including their
 	employee number, last name, first name, and department name.*/
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+WHERE de.dept_no IN ('d007', 'd005');
 
 /*8 List the frequency counts, in descending order, of all the employee last names 
 	(that is, how many employees share each last name).*/
-
-
+SELECT last_name, COUNT(*) as frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
 
