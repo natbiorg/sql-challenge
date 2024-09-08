da7feefb3a90d159846f3aa10bcad0f22db37485
Natalie <natalierg1199@gmail.com>
2024-09-07 22:17:39 -0500

    progress

--- nosql-challenge2.sql
+++ nosql-challenge2.sql
@@ -77,23 +77,57 @@ REFERENCES "employees" ("emp_no");
 SELECT * FROM dept_emp;
 
 /* If i were to continue the rest of the challenge */ 
-//1 
+/*1 List the employee number, last name, first name, sex, and salary of each employee. */
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s on e.emp_no = s.emp_no;
 
-//2 
+/*2 List the first name, last name, and hire date for the employees who were hired in 1986. */
 SELECT first_name, last_name, hire_date
 FROM employees 
 	WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-//3 
-SELECT d.dept_no, e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees e
-JOIN salaries s on e.emp_no = s.emp_no;
+/*3 List the manager of each department along with their department number,
+department name, employee number, last name, and first name. */
+SELECT 
+    dm.dept_no,
+    d.dept_name,
+    dm.emp_no,
+    e.last_name,
+    e.first_name
+FROM 
+    departments d
+JOIN 
+    dept_manager dm ON d.dept_no = dm.dept_no
+JOIN 
+    employees e ON dm.emp_no = e.emp_no;
 
+/*4 List the department number for each employee along with that employee’s employee number,
+last name, first name, and department name.*/
+SELECT 
+    d.dept_no, 
+    e.emp_no, 
+    e.last_name, 
+    e.first_name, 
+    d.dept_name
+FROM 
+    departments d
+JOIN 
+    dept_manager dm ON d.dept_no = dm.dept_no
+JOIN 
+    employees e ON dm.emp_no = e.emp_no;
 
+/*5 List first name, last name, and sex of each employee 
+	whose first name is Hercules and whose last name begins with the letter B. */
 
+/*6 List each employee in the Sales department, including their employee number, 
+	last name, and first name.*/
+
+/*7 List each employee in the Sales and Development departments, including their
+	employee number, last name, first name, and department name.*/
+
+/*8 List the frequency counts, in descending order, of all the employee last names 
+	(that is, how many employees share each last name).*/
 
 
 
